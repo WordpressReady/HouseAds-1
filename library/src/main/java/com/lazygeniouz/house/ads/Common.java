542b7f78e36d36d6fc3ffbc80e1d0f124e23c1f4
Fernando Zorrilla de S.M <admin@wordpressready.com>
2019-07-05 06:36:00 -0300

    - Common class to setup some constants. Currently this not belong to the library, this is a mistake to fix - Changed some initial default values - Changed constructor to include mandatory parameters (json Url) - Added some log info -Adding ad filtering for category or for disabled ads. - Fixed bug when getting non-existent rating -Refactored and simplified example

---
+++ library/src/main/java/com/lazygeniouz/house/ads/Common.java
@@ -0,0 +1,10 @@
+package com.lazygeniouz.house.ads;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Common {
+    static String NO_CATEGORY ="[no category]";
+    public static ArrayList<String> categories = new ArrayList<String>(Arrays.asList("pregnancy","quizz")); //List of available categories to be filtered for THIS app.
+
+}
